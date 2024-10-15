83b22407f5d3ec1402ff2513c9279bb20a578e7a
Vando <vandomeri@gmail.com>
2024-10-15 12:12:32 +0300

    ws fix

--- websocket/server.js
+++ websocket/server.js
@@ -1,12 +1,11 @@
 const WebSocket = require('ws')
 
 const server = new WebSocket.Server({ port: 3000 })
-console.log(server.clients)
 server.on("connection", connect => {
 
     connect.on('message', message => {
         const msg = JSON.parse(message.toString())
-        console.log(client)
+
         server.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN)
                 client.send(`${msg.nick}: ${msg.message}`)
